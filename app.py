d8d88aab6b447393b928ebd99b8d21a0fd639c38
Jef-ux <aalexisbears@gmail.com>
2022-07-13 02:17:12 +0100

    new

--- app.py
+++ app.py
@@ -99,10 +99,7 @@ def retrieve_all_templates():
 @jwt_required()
 def retrieve_one_template(id):
 
-	get_jwt_identity()
-
-	template = templates_collection.find_one({"_id": id})
-	template['_id'] = str(template['_id'])
+	template = templates_collection.find_one({"user_id":get_jwt_identity()})
 	
 	if template:
 		return make_response(template), 200
